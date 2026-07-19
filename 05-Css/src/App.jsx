09fadfb70901d88b8e8b5cd7cb606985b55bca5e
Sumit Kumar <creatinggsumit@gmail.com>
2026-07-19 13:28:12 +0530

    Add 05-Css folder

---
+++ 05-Css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Button from './components/button/Button'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Button/>
+    </>
+  )
+}
+
+export default App
---
+++ 05-Css/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const button = () => {
+  return (
+    <div className={styles.btn}>
+        "this is button component"
+    </div>
+  )
+}
+
+export default button
