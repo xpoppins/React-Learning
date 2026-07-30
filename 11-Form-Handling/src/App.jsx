14c88728f23d04d2e93be5c9d87b5334ba426139
Sumit Kumar <creatinggsumit@gmail.com>
2026-07-30 22:09:47 +0530

    learning in progress

---
+++ 11-Form-Handling/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder='Enter your name' />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
